c4679b4edc9913c0cdaa91f5ec2e745dad8a7848
Georgiy Kazarin <kazarin.gm@phystech.edu>
2025-03-31 01:30:11 +0300

    Good but not colorful image

--- main.cpp
+++ main.cpp
@@ -2,14 +2,14 @@
 #include <math.h>
 #include <SFML/Graphics.hpp>
 
-const double    X0              = 300,
+const double    X0              = 400,
                 Y0              = 300,
                 R0_2            = 100;
 
 const int       N_ITERATIONS    = 256,
                 WIDTH           = 600,
                 HEIGHT          = 600,
-                PXL_PER_UNIT    = 150;
+                PXL_PER_UNIT    = 190;
 
 const char*     WINDOW_NAME     = "The Mandelbrot set";
 
@@ -33,20 +33,21 @@ int main()
             for (double yi = 0; yi < HEIGHT; ++yi)
             {
                 double xi0 = (xi - X0) / PXL_PER_UNIT,
-                    yi0 = (yi - Y0) / PXL_PER_UNIT,
-                    x   = xi0,
-                    y   = yi0;
+                       yi0 = (yi - Y0) / PXL_PER_UNIT,
+                       x   = xi0,
+                       y   = yi0;
 
                 int i = 0;
                 for ( ; i < N_ITERATIONS && x*x + y*y < R0_2; ++i)
                 {
+                    double old_x = x;
                     x = x*x - y*y + xi0;
-                    y = 2*x*y     + yi0;
+                    y = 2*old_x*y + yi0;
                 }
 
                 if (i == N_ITERATIONS) continue;
 
-                image.setPixel(xi, yi, sf::Color(i, i/2, 255 - i));
+                image.setPixel(xi, yi, sf::Color(i, 0, sqrt(255 - i)));
             }
         }
 
